866551ec754bd9855685c873f5982f9c5d011657
Rosa Kang <therosessom@gmail.com>
2022-05-27 07:47:17 -0700

    05-26-2022

---
+++ names.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    string names[] = {"a", "b", "c", "d", "e", "f", "g" };
+
+    if(strcmp(names[i], "f")){
+        printf("Found\n");
+        return 0;
+    }
+    printf("Not found\n");
+    return 1;
+}
