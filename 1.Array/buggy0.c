063774ad32aa31ad656e9604d65d4c8d3983e41a
Rosa Kang <therosessom@gmail.com>
2022-05-27 07:14:47 -0700

    cs50

---
+++ 1.Array/buggy0.c
@@ -0,0 +1,8 @@
+int main(voide)
+{
+    for(int i=0; i<10; i++) 
+    {
+        printf("i is now %i\n", i);
+        printf("#\n");
+    }
+}
---
+++ 1.Array/hello.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    int x= get_int("x:");
+    int y =get_int("y:");
+    printf("%i\n, x+y");
+}
+
+int i =0;
+while(i<=3){
+    printf("");
+    i+=1;
+}
+
+for(int i=0; i<3; i++){
+    printf();
+}
